3d81ca7d8c0da7382b2d457188f6b96abf8d938c
Jiyong-C-Lee <liramus214@gmail.com>
2017-06-30 01:45:18 +0900

    initial
    initial

---
+++ app/src/main/java/com/example/administrator/recyclerviewex/PostRecyclerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.administrator.recyclerviewex;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Administrator on 2017-06-29.
+ */
+
+public class PostRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    ArrayList<PostItems> item_list;
+    View.OnClickListener click_listener;
+
+
+    public PostRecyclerAdapter(ArrayList<PostItems> item_list, View.OnClickListener click_listener) {
+        this.item_list = item_list;
+        this.click_listener = click_listener;
+
+    }
+
+    @Override
+    public PostRecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_recyclerview_item,parent,false);
+        view.setOnClickListener(click_listener);
+        PostRecyclerViewHolder view_holder = new PostRecyclerViewHolder(view);
+
+
+        return view_holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        PostRecyclerViewHolder mholder = (PostRecyclerViewHolder) holder;
+        mholder.img.setImageResource(item_list.get(position).img);
+        mholder.text.setText(item_list.get(position).text);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        //아이템 카운터
+        return item_list != null ? item_list.size() : 0;
+    }
+}
