e2d60450a001973844669e0ce19c2b0461ffaffb
Jiyong-C-Lee <liramus214@gmail.com>
2017-06-30 16:59:07 +0900

    HomecardFragmentInitial
    HomecardFragmentInitial

---
+++ HomeCardFragment/app/src/main/java/com/example/administrator/homecardfragment/HomeCardFragment.java
@@ -0,0 +1,13 @@
+package com.example.administrator.homecardfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeCardFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_card_fragment);
+    }
+}
